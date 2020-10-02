92e5cf120d5d11b02b521fa31c50c228d0c4434d
rita <ritadhge.gmail.com>
2020-10-02 23:28:41 +0530

    add role model

--- Newapp/models.py
+++ Newapp/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
 class Userinfo(models.Model):
+    """create a class for userinfo for resistration"""
     username=models.CharField(max_length=65)
     email=models.EmailField(max_length=30)
     password=models.CharField(max_length=65)
 
 
-# class Roles(models.Model):
-#     roles=models.CharField(max_length=65)
-#     user=models.ManyToManyField(Userinfo)
+class Roles(models.Model):
+    """create class for roles"""
+    roles=models.CharField(max_length=65)
+    user=models.ForeignKey(Userinfo,on_delete=models.CASCADE,related_name='roles',null=True)
 
 
 
