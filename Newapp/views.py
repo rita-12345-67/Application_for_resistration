899e7a478c3410dbd6958e96f92243f735416c7b
rita <ritadhge.gmail.com>
2020-10-02 23:32:30 +0530

    create viewset for userinfo

--- Newapp/views.py
+++ Newapp/views.py
@@ -4,5 +4,6 @@ from rest_framework.viewsets import ModelViewSet
 from Newapp.appserlizer import Userserlizer
 from Newapp.models import Userinfo
 class Userview(ModelViewSet):
+    """ create viewset for User"""
     queryset = Userinfo.objects.all()
     serializer_class = Userserlizer
