8ba1a922d4b910f8ff9daeceae45d3d624e6a73f
rita <ritadhge.gmail.com>
2020-10-03 17:36:11 +0530

    added model fields

--- Newapp/models.py
+++ Newapp/models.py
@@ -6,12 +6,31 @@ class Userinfo(models.Model):
     email=models.EmailField(max_length=30)
     password=models.CharField(max_length=65)
 
-
 class Roles(models.Model):
     """create class for roles"""
     roles=models.CharField(max_length=65)
     user=models.ForeignKey(Userinfo,on_delete=models.CASCADE,related_name='roles',null=True)
 
+
+
+
+# class Role(models.Model):  # to assign roles
+#     role = models.CharField(max_length=50)
+#     active = models.CharField(max_length=50, default='Y')
+#     userrefs = models.ManyToManyField(UserInfo)
+#     activeRoles = ActiveRoles()
+#     roles = models.Manager()
+#
+#     @staticmethod
+#     def dummyrole():
+#         return Role(role='staff')
+#
+#
+# class LoginInfo(models.Model):  # to maintain credentails
+#     username = models.CharField(max_length=50)
+#     password = models.CharField(max_length=50)
+#     tokenval = models.CharField(max_length=50)
+#     userref = models.OneToOneField(UserInfo, models.CASCADE)
 
 
 
