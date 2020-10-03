75004b27af4ce91d34cb71d033ea222de181157a
rita <ritadhge.gmail.com>
2020-10-03 16:54:51 +0530

    change in the signup and change password

--- Newapp/appserlizer.py
+++ Newapp/appserlizer.py
@@ -1,7 +1,31 @@
 from rest_framework.serializers import ModelSerializer
 from Newapp.models import Userinfo
 
-class Userserlizer(ModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = Userinfo
-        fields= '__all__'
+        fields = ('id', 'username', 'email')
+
+# Register Serializer
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = Userinfo
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = Userinfo.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        # "token": AuthToken.objects.create(user)[1]
+        return user
+
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+
+    """
+    Serializer for password change endpoint.
+    """
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
